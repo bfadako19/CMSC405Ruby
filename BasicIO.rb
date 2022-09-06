1ec1a499f534c2b1e09604350fb0a0ba8b62c7b9
Bryce Fadako <fadakob@chc.edu>
2022-09-06 10:47:35 -0400

    September 6,2022

---
+++ BasicIO.rb
@@ -0,0 +1,10 @@
+# Ruby provides a whole set of I/O mehtods in the Kernel module
+# A Ruby module is like a Java interface 
+
+# The print mehtod prints each parameter its passed to standard out (stdout)
+# It does not append a new line to the end of its output
+print "cat", [1,2,3], 99, "\n"
+print "cat", [1,2,3], 99, "\n"
+
+# Observations: The arguments to the print method were not enclosed in parenthesis
+# Delimiters like the semi-colon, weren't needed at the end of the lines of code
--- BookInStock.rb
+++ BookInStock.rb
@@ -8,6 +8,8 @@ class BookInStock
 end
 
 def to_s
-    "IBSN #(@ibsn), Price: #(@price)"
+    "IBSN; #{@ibsn}, Price: #{@price}"
     end
 end
+b1 = BookInStock.new('ibsn', 25.50)
+puts b1.to_s
